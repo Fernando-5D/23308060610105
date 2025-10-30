ea641a8a99c3426d90ab477931efb88116701957
Fernando-5D <23308060610105@cetis61.edu.mx>
2025-10-30 10:17:41 -0500

    commit

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return
+def form():
+    return render_template("form.html")
+
+@app.route("/clasificar-macro", methods=("GET", "POST"))
+def clasificar():
+    if request.method == "POST":
+        nombre = request.form("nombre")
+        grasas = request.form("grasas") * 9
+        proteinas = request.form("proteinas") * 4
+        carbohidratos = request.form("carbohidratos") * 4
+        
+        session["alimentos"]
 
 if __name__ == "__main__":
     app.run(debug=True)
